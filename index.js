ef3895a247f6b660b8df4e5f5be2aa2204113ddd
dohaattaria <dohafatima484@gmail.com>
2024-10-01 23:41:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// chapter 01
+// alert
+
+alert("WELL COME TO MY WEBSITE!" );
+
+alert("HELLO WORLD");
+
+// chapter 02
+//   declares a variable
+
+var tittle = "Hello, world!";
+alert(tittle);
+
+// Declare a variable age
+var age;
+age = 18;
+alert(age);
+
+
+// Declare a variable greeting
+var greeting = "Good morning!";
+alert(greeting);
+
+
+greeting = "Good night!";
+alert(greeting);
+
+
+// Declare a variable called Color
+var Color = "pink";
+alert("My favorite color is " + Color + ".");
+
+
+
